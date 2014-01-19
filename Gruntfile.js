d130d13df76bea535bac508da013c3760a2dca0c
Abraham Kuri <kurenn@icalialabs.com>
2014-01-19 16:52:11 -0600

    Adds bower

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,14 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jasmine: {
-      components: {
-        src: [
-          'src/js/*.js'
-        ],
         options: {
           specs: 'specs/js/*Spec.js',
           keepRunner : true,
+          vendor: "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"
           //helpers: 'test/spec/*.js'
         }
       }
-    }
   });
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
   grunt.registerTask('travis', ['jasmine']);
-}
+};
